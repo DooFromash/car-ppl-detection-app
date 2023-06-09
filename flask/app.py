7fd90c4e2b571f6f2925fc0f6f92ca1561d8685c
DooFromash <garciaadave@gmail.com>
2023-06-09 09:30:52 +0200

    app that returns prediction of an image in text

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask,render_template,redirect,request, url_for, jsonify
+from ultralytics import YOLO
+import json
+import pandas as pd
+import torch
+
+# Model
+model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
+
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello, World!"
+    return render_template("model.html")
+
+@app.route("/home")
+def home():
+    return redirect('/')
+
+@app.route('/submit',methods=['POST'])
+def submit_data():
+    
+    f=request.files['userfile']
+    f.save(f.filename)
+    results = model(f.filename)
+   
+
+    
+    
+    # response = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+    response = results
+    print(response)
+
+   
+
+    return render_template('model.html', response = response)
+
 
 if __name__ =="__main__":
     
