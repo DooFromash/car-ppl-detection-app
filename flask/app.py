9239623c60674274c95fe80318e6be792e0b86a5
DooFromash <garciaadave@gmail.com>
2023-06-10 22:58:38 +0200

    v4: model printing image with results, updated Front end, added a function to delete temp files of predictions after closing the server

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,25 @@
-from flask import Flask,render_template,redirect,request, url_for, jsonify
+from flask import Flask,render_template,redirect,request
 from ultralytics import YOLO
-import json
 import pandas as pd
 import torch
 import numpy as np 
-import cv2
-import os
 from tempfile import NamedTemporaryFile
+import os
+import atexit
+import shutil
+
 
 # Model
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
 
-temp_folder = "temp_images"
-temp_file_path = os.path.join(temp_folder, "uploaded_image.jpg")
-
 app = Flask(__name__)
+
+runs_folder = os.path.join("static", "runs")
+
+@atexit.register
+def delete_runs_folder_at_exit():
+    if os.path.exists(runs_folder):
+        shutil.rmtree(runs_folder)
 
 
 @app.route('/')
@@ -34,12 +39,23 @@ def submit_data():
     
     f.save(f.filename)
     results = model(f.filename)
+     # Generate a unique filename for the saved results
+    output_filename = "runs/detect/exp"
+    output_filepath = os.path.join("static", output_filename)
+    
+    # Save the results to the static folder with the specified filename
+    results.save(save_dir=output_filepath)
+
+    folder_path = 'static/runs/detect'
+    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
+    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
+    image_path = folder_path+'/'+latest_subfolder+'/'+f.filename
     
     
     
     response_details = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
     response = results
-    image_path = results.show()     
+       
     
     print(response)
 
